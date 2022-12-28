94e0b93b816a3b242cda683553b1fe2033e37018
ujangaripin24 <ujangarifin06@gmail.com>
2022-12-28 21:03:39 +0700

    register page has been installed

---
+++ frontend/src/components/RegisterPage.vue
@@ -0,0 +1,51 @@
+<template>
+    <h2>Register page</h2>
+    <input 
+        type="email"
+        name="email"
+        v-model="email"
+        placeholder="Email Anda..."
+    />
+    <br>
+    <input 
+        type="password"
+        name="password"
+        v-model="password"
+        placeholder="Password Anda"
+    />
+    <br>
+    <button
+        @click="register"
+    >Register</button>    
+</template>
+
+<script>
+import AuthenticationService from './../../../backend/src/services/AuthenticationService.js'
+    export default{
+        data(){
+            return{
+                email: 'admin',
+                password: '123'
+            }
+        },
+        watch:{
+            email(value){
+                console.log('email telah berubah menjadi', value)
+            }
+        },
+        methods:{
+            async register(){
+                const response = await AuthenticationService.register({
+                    email: this.email,
+                    password: this.password
+                })
+                console.log(response.data)
+            }
+        },
+        mounted() {
+            setTimeout(() => {
+                this.email = 'hello world'
+            }, 2000)
+        }
+    }
+</script>
---
+++ frontend/src/views/RegisterView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <RegisterPage msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import RegisterPage from '@/components/RegisterPage.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    RegisterPage
+  }
+}
+</script>
